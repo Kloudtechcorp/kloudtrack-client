d8cea2979fa84f6ebe56f950c4e8f839fc35a3e4
Regee <regeecasana57@gmail.com>
2024-11-12 22:45:24 +0800

    station list refactoring

--- src/_root/pages/AdminSettings.tsx
+++ src/_root/pages/AdminSettings.tsx
@@ -6,56 +6,70 @@ import AddPsgc from "./adminpages/AddPsgc";
 import AddStationType from "./adminpages/AddStationType";
 import NotFound from "@/components/shared/NotFound";
 import { Card } from "@/components/ui/card";
-import StationList from "./adminpages/StationList";
 import { UserControl } from "./adminpages/UserControl";
+import { StationList } from "./adminpages/StationList";
 
 const AdminSettings = () => {
   const { user } = useUserContext();
 
   return user.role === "ADMIN" ? (
-    <Tabs
-      defaultValue="account"
-      className="w-full flex-col dark:bg-slate-950 bg-[#F6F8FC] p-5 rounded-2xl overflow-x-auto min-x-[420px]"
-    >
-      <TabsList className="flex flex-row justify-start container">
-        <TabsTrigger value="account">Account Creation</TabsTrigger>
-        <TabsTrigger value="psgc">Add PSGC</TabsTrigger>
-        <TabsTrigger value="stationType">Add Station Type</TabsTrigger>
-        <TabsTrigger value="station">Station Registration</TabsTrigger>
-        <TabsTrigger value="list">Station List</TabsTrigger>
-        <TabsTrigger value="users">User Control</TabsTrigger>
-      </TabsList>
-      <TabsContent value="account" className="container px-0">
-        <Card>
-          <UserCreation />
-        </Card>
-      </TabsContent>
-      <TabsContent value="psgc" className="container px-0">
-        <Card>
-          <AddPsgc />
-        </Card>
-      </TabsContent>
-      <TabsContent value="stationType" className="container px-0">
-        <Card>
-          <AddStationType />
-        </Card>
-      </TabsContent>
-      <TabsContent value="station" className="container px-0">
-        <Card>
-          <StationRegistration />
-        </Card>
-      </TabsContent>
-      <TabsContent value="list" className="container px-0">
-        <Card>
-          <StationList />
-        </Card>
-      </TabsContent>
-      <TabsContent value="users" className="container px-0">
-        <Card className="px-4">
-          <UserControl />
-        </Card>
-      </TabsContent>
-    </Tabs>
+    <div className="flex w-full h-full dark:bg-slate-950 bg-[#F6F8FC] p-2">
+      <Tabs
+        defaultValue="account"
+        className="w-full flex lg:flex-row flex-col p-3 rounded-2xl gap-5 justify-center container"
+      >
+        <TabsList className="flex flex-col justify-start gap-3 w-full lg:w-1/5 h-28 p-5 container">
+          <TabsTrigger value="account" className="w-full">
+            Account Creation
+          </TabsTrigger>
+          <TabsTrigger value="psgc" className="w-full">
+            Add PSGC
+          </TabsTrigger>
+          <TabsTrigger value="stationType" className="w-full">
+            Add Station Type
+          </TabsTrigger>
+          <TabsTrigger value="station" className="w-full">
+            Station Registration
+          </TabsTrigger>
+          <TabsTrigger value="list" className="w-full">
+            Station List
+          </TabsTrigger>
+          <TabsTrigger value="users" className="w-full">
+            User Control
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="account" className="container px-0">
+          <Card>
+            <UserCreation />
+          </Card>
+        </TabsContent>
+        <TabsContent value="psgc" className="container px-0">
+          <Card>
+            <AddPsgc />
+          </Card>
+        </TabsContent>
+        <TabsContent value="stationType" className="container px-0">
+          <Card>
+            <AddStationType />
+          </Card>
+        </TabsContent>
+        <TabsContent value="station" className="container px-0">
+          <Card>
+            <StationRegistration />
+          </Card>
+        </TabsContent>
+        <TabsContent value="list" className="container px-0">
+          <Card>
+            <StationList />
+          </Card>
+        </TabsContent>
+        <TabsContent value="users" className="container px-0">
+          <Card className="px-4">
+            <UserControl />
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
   ) : (
     <div className="rounded-xl bg-[#F6F8FC] dark:bg-secondary w-full h-full">
       <NotFound />

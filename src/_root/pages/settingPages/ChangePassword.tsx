bb284e0bb0ea19e2393c28a471508761d0e52de5
AustinSimbol <austinvincesimbol@gmail.com>
2024-11-22 17:42:33 +0800

    change password bg fix

--- src/_root/pages/settingPages/ChangePassword.tsx
+++ src/_root/pages/settingPages/ChangePassword.tsx
@@ -89,135 +89,139 @@ const ChangePassword = () => {
   const isPasswordValid = Object.values(passwordCriteria).every(Boolean);
 
   return (
-    <Form {...form}>
-      <Card className="px-5 w-full h-full">
-        <span className="flex py-5 font-bold text-lg">
-          Add information for new user
-        </span>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          <div className="flex flex-col gap-5">
-            <FormField
-              control={form.control}
-              name="currentPassword"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Current Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="password"
-                      placeholder="Enter your current password."
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage className="shad-form_message" />
-                </FormItem>
-              )}
-            />
+    <div>
+      <Form {...form}>
+        <Card className="px-5 w-full h-full">
+          <span className="flex py-5 font-bold text-lg">
+            Add information for new user
+          </span>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-5">
+              <FormField
+                control={form.control}
+                name="currentPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Current Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Enter your current password."
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage className="shad-form_message" />
+                  </FormItem>
+                )}
+              />
 
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>New Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      type="password"
-                      placeholder="Enter new password"
-                      onChange={(e) => {
-                        field.onChange(e);
-                        checkPasswordStrength(e.target.value); // Check password as the user types
-                      }}
-                    />
-                  </FormControl>
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>New Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="password"
+                        placeholder="Enter new password"
+                        onChange={(e) => {
+                          field.onChange(e);
+                          checkPasswordStrength(e.target.value); // Check password as the user types
+                        }}
+                      />
+                    </FormControl>
 
-                  <FormMessage className="shad-form_message" />
-                </FormItem>
-              )}
-            />
+                    <FormMessage className="shad-form_message" />
+                  </FormItem>
+                )}
+              />
 
-            <FormField
-              control={form.control}
-              name="confirmPassword"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Confirm Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="password"
-                      placeholder="Re-enter password"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage className="shad-form_message" />
-                </FormItem>
-              )}
-            />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Re-enter password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage className="shad-form_message" />
+                  </FormItem>
+                )}
+              />
 
-            <div className="mt-2 text-sm">
-              <ul>
-                <li
-                  className={
-                    passwordCriteria.length ? "text-green-500" : "text-red-500"
-                  }
+              <div className="mt-2 text-sm">
+                <ul>
+                  <li
+                    className={
+                      passwordCriteria.length
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }
+                  >
+                    {passwordCriteria.length ? "✔" : "✘"} Must be a minimum of 8
+                    characters.
+                  </li>
+                  <li
+                    className={
+                      passwordCriteria.uppercase
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }
+                  >
+                    {passwordCriteria.uppercase ? "✔" : "✘"} Must contain at
+                    least one uppercase letter.
+                  </li>
+                  <li
+                    className={
+                      passwordCriteria.lowercase
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }
+                  >
+                    {passwordCriteria.lowercase ? "✔" : "✘"} Must contain at
+                    least one lowercase letter.
+                  </li>
+                  <li
+                    className={
+                      passwordCriteria.digit ? "text-green-500" : "text-red-500"
+                    }
+                  >
+                    {passwordCriteria.digit ? "✔" : "✘"} Must contain at least
+                    one digit.
+                  </li>
+                  <li
+                    className={
+                      passwordCriteria.specialChar
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }
+                  >
+                    {passwordCriteria.specialChar ? "✔" : "✘"} Must contain at
+                    least one special character.
+                  </li>
+                </ul>
+              </div>
+              <div className="w-full flex justify-end">
+                <Button
+                  type="submit"
+                  className={`my-5 dark:bg-blue-200`}
+                  disabled={!isPasswordValid}
                 >
-                  {passwordCriteria.length ? "✔" : "✘"} Must be a minimum of 8
-                  characters.
-                </li>
-                <li
-                  className={
-                    passwordCriteria.uppercase
-                      ? "text-green-500"
-                      : "text-red-500"
-                  }
-                >
-                  {passwordCriteria.uppercase ? "✔" : "✘"} Must contain at least
-                  one uppercase letter.
-                </li>
-                <li
-                  className={
-                    passwordCriteria.lowercase
-                      ? "text-green-500"
-                      : "text-red-500"
-                  }
-                >
-                  {passwordCriteria.lowercase ? "✔" : "✘"} Must contain at least
-                  one lowercase letter.
-                </li>
-                <li
-                  className={
-                    passwordCriteria.digit ? "text-green-500" : "text-red-500"
-                  }
-                >
-                  {passwordCriteria.digit ? "✔" : "✘"} Must contain at least one
-                  digit.
-                </li>
-                <li
-                  className={
-                    passwordCriteria.specialChar
-                      ? "text-green-500"
-                      : "text-red-500"
-                  }
-                >
-                  {passwordCriteria.specialChar ? "✔" : "✘"} Must contain at
-                  least one special character.
-                </li>
-              </ul>
+                  {isPending ? "Loading..." : "Submit"}
+                </Button>
+              </div>
             </div>
-            <div className="w-full flex justify-end">
-              <Button
-                type="submit"
-                className={`my-5 dark:bg-blue-200`}
-                disabled={!isPasswordValid}
-              >
-                {isPending ? "Loading..." : "Submit"}
-              </Button>
-            </div>
-          </div>
-        </form>
-      </Card>
-    </Form>
+          </form>
+        </Card>
+      </Form>
+    </div>
   );
 };
 

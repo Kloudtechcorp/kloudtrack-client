9864a1d64374174838716b570a136191d0c5840e
rekasa7000 <regeecasana57@gmail.com>
2025-02-03 14:25:02 +0800

    stacked graph bug fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -317,6 +317,8 @@ export const getFormattedDataset = (data: GraphData[]): GraphData[] => {
     const formattedDate = new Date(item.recorded).toLocaleString("en-US", {
       month: "long",
       day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
       hour12: false,
     });
     const datetimeWithAt = formattedDate.replace(",", " at");
